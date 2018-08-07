cb5f79407466f13a9a0a69d85f1b665f2ae8426f
wyk <wyk1121@qq.com>
2018-08-07 13:49:50 +0800

    second merge

--- src/main/java/com/wizz/hospitalSell/dao/UserInfoDao.java
+++ src/main/java/com/wizz/hospitalSell/dao/UserInfoDao.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface UserInfoDao extends JpaRepository<UserInfo,Integer> {
-    UserInfo findByUserName(String userName);
     UserInfo findByUserId(Integer userId);
     UserInfo findByUserOpenid(String userOpenid);
+    Boolean existsByUserName(String userName);
+
+    @Override
+    <S extends UserInfo> S save(S s);
 }
--- src/test/java/com/wizz/hospitalSell/UserControllerTest.java
+++ src/test/java/com/wizz/hospitalSell/UserControllerTest.java
@@ -1,4 +1,36 @@
 package com.wizz.hospitalSell;
 
+
+
+import com.wizz.hospitalSell.controller.UserController;
+import com.wizz.hospitalSell.dao.UserInfoDao;
+import com.wizz.hospitalSell.domain.UserInfo;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
 public class UserControllerTest {
+
+    @Autowired
+    UserController userController;
+
+    @Test
+    public void test(){
+        String userIcon="212121";
+        String userName="wyk";
+        String userAddress="232323";
+        String userPhone="121331311";
+
+
+        System.out.println(userController.info(userName,userIcon,userAddress,userPhone));
+
+    }
 }
